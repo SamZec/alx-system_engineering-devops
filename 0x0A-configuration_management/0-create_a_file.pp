c8a0e07ec6f8d1a961587b52fa5cb3a2722e4030
SamZec <samzec4@gmail.com>
2023-06-24 09:39:58 +0000

    0. Create a file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,11 @@
+# 0-create_a_file.pp
+# create a file in /tmp.
+
+file {'school':
+  ensure  => 'present',
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
+}
